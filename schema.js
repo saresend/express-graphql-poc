246766488be609a1f404d1d8a4199d9b0b22f4f4
saresend <saresend@usc.edu>
2019-09-19 10:12:32 -0700

    beginning to work on joinMonster. might integrate knex as well

--- schema.js
+++ schema.js
@@ -1,6 +1,27 @@
 var graphqlObj = require('graphql');
 var joinMonster = require('joinMonster');
 
+
+const User = new graphqlObj.GraphQLObjectType({
+  name: 'User',
+  sqlTable: 'users',
+  uniqueKey: 'id',
+  fields: () => ({
+    id: {
+      type: graphqlObj.GraphQLInt,
+    },
+    email: {
+      type: graphqlObj.GraphQLString,
+    },
+    firstName: {
+      type: graphqlObj.GraphQLString,
+    },
+    lastName: {
+      type: graphqlObj.GraphQLString,
+    },
+  }),
+})
+
 var schema = new graphqlObj.GraphQLSchema({
   query: new graphqlObj.GraphQLObjectType({
     name: 'RootQueryType',
@@ -11,6 +32,20 @@ var schema = new graphqlObj.GraphQLSchema({
           return 'world';
         },
       },
+      user: {
+        type: User,
+        args: {
+          id: { type: GraphQLInt },
+        },
+        where: (usersTable, args, context) => {
+          if (args.id) return `${usersTable}.id = ${args.id}`
+        },
+        resolve: (parents, args, context, resolveInfo) => {
+          return joinMonster(resolveInfo, {}, sql => {
+            return knex.raw(sql);
+          })
+        }
+      }
     },
   }),
 });
